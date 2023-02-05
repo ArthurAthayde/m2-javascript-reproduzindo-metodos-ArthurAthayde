62749cc8733cbd89148e1c381a64fbb8ff70c47e
ArthurAthayde <arthur.athaydee@gmail.com>
2023-02-05 17:27:48 -0300

    métodos - testes 16/16

--- src/scripts/metodos.js
+++ src/scripts/metodos.js
@@ -1,25 +1,64 @@
-function newMap() {
-  // Reproduza aqui o seu método map
-}
+function newMap(array, callback) {
+  let outputArray = [];
+  for (let i = 0; i < array.length; i++) {
 
-function newFilter() {
-  // Reproduza aqui o seu método filter
-}
 
-function newFind() {
-  // Reproduza aqui o seu método find
-}
+    let callbackReturn = callback(array[i], i, array);
 
-function newReduce() {
-  // Reproduza aqui o seu método Reuce
+    outputArray.push(callbackReturn)
+  }
+  return outputArray;
 }
 
 
 
+function newFilter(array, callback) {
+  let outputFiltered = []
+
+  for (let i = 0; i < array.length; i++) {
+    let callbackFilter = callback(array[i], i, array);
+
+    if (callbackFilter) {
+      outputFiltered.push(array[i]);
+    }
+  }
+  return outputFiltered;
+}
 
 
 
-// Não alterar o código abaixo
+function newFind(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    let callbackFind = callback(array[i], i, array);
+    if (callbackFind) {
+      return array[i]
+    }
+  }
+}
+
+function newReduce(array, callback, initialValue) {
+  let acc;
+  let initialIndex;
+
+  if (initialValue != undefined) {
+    acc = initialValue;
+    initialIndex = 0;
+  } else {
+    acc = array[0];
+    initialIndex = 1;
+  }
+
+  for (let i = initialIndex; i < array.length; i++) {
+    let callbackReduce = callback(acc, array[i], i, array);
+
+    acc = callbackReduce;
+    
+  }
+  return acc;
+}
+
+
+
 
 export {
   newMap,
